26fe68b3c4f8a4436d9e5ecff4cf512e5a9434af
noraleonte <noraleonte00@gmail.com>
2023-03-17 16:49:04 +0200

    set up api for webhooks

--- src/pages/api/hubspot/index.ts
+++ src/pages/api/hubspot/index.ts
@@ -1,11 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+type ResponseType = {
+  eventId: string
+  subscriptionId: string
+  portalId: string
+  appId: string
+  occurredAt: string
+  subscriptionType: string
+  attemptNumber: string
+  objectId: string
+  changeFlag: string
+  changeSource: string
+  sourceId: string
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     /* eslint-disable no-console */
-    console.log(req)
-    res.status(200).json({ message: 'success' })
+    if (req.body) {
+      const event =
+        req.body.find(
+          (e: ResponseType) => e.subscriptionType === 'company.propertyChange'
+        ) ||
+        req.body.find(
+          (e: ResponseType) => e.subscriptionType === 'company.deletion'
+        )
+      const { subscriptionType } = event
+
+      if (subscriptionType === 'company.propertyChange') {
+        console.log('set/create doc', event)
+      } else {
+        console.log('delete item', event)
+      }
+    }
+    res.status(200).json({ message: 'Success' })
   } else {
-    console.log('FAIL')
+    res.status(400).json({ message: 'Failed' })
   }
 }
